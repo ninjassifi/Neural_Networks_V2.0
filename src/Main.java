6d3c004147ad47cc387e7f857bbbfb369e56b5d1
ninjassifi <nicholas.maksimov@gmail.com>
2022-12-27 12:10:12 -0800

    Version 1.1

--- src/Main.java
+++ src/Main.java
@@ -1,98 +1,101 @@
 import java.util.*;
 
 public class Main {
-    static int inputNodes = 2;
-    static int outputNodes = 2;
+    static int inputNodes = 8;
+    static int outputNodes = 8;
     static int generations = 10000;
-    static int[] layers = new int[] {inputNodes, 10, 10, outputNodes};
+    // In order to add layers, just add a number in the middle and add a comma
+    static int[] layers = new int[] {inputNodes, 10, outputNodes};
     static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
         NeuralNetwork net = null;
-        for(int i = 0; i < generations; i++)
-            net = train(net, 100, 0, 3);
+        for (int i = 0; i < generations; i++)
+            net = train(net, 100);
         System.out.println("done");
+
         // test the neural networks
         while(true) {
-
+            // Get user number
             int num = scanner.nextInt();
+
+            // Initialize input array
             float[] input = new float[inputNodes];
-            boolean[] boolArray = new boolean[input.length];
+
+            // This is temp boolean array to change into 1s and 0s
+            boolean[] boolArray;
             boolArray = toBin(num, inputNodes);
             for(int i = 0; i < inputNodes; i++) {
                 input[i] = (float)(boolArray[i] ? 1 : 0);
             }
+            System.out.println(Arrays.toString(input));
             float[] out = net.feedForward(input);
             System.out.println("output of array: " + Arrays.toString(out));
         }
     }
-    static NeuralNetwork train(NeuralNetwork net, int populationSize, int min, int max) {
-        // Random number var
-        int randomNumber;
-        boolean[] boolArray;
-        float[] binArray;
-        int lastDeviation = 10000;
-        int deviation = 0;
-        int bestNetwork = 0;
+    // Training is supposed to:
+    // 1. create a duplicate network with a little change
+    // 2. check if it's better
+    // 3. return the best one
+    static NeuralNetwork train(NeuralNetwork net, int popSize) {
+
+        // Make an input as long as there are input nodes
+        float[] input = new float[inputNodes];
+
+        // Out
         float[] out;
-        // Generate network
+
+        // Random Number stuff
+        float randomNumber;
+
+        // Best network stuff
+        int bestNetwork = 0;
+
+        // New list of networks
+        List<NeuralNetwork> nets = new ArrayList<>();
+
+        // Check if the neuralNetwork doesn't exist, if not, make one
         if (net == null) {
             net = new NeuralNetwork(layers);
         }
-        List<NeuralNetwork> nets = new ArrayList<NeuralNetwork>();
-        for (int i = 0; i < populationSize; i++) {
-            // Add neuralNetwork to nets
-            nets.add(new NeuralNetwork(net));
+        for (int i = 0; i < popSize; i++) {
+            // Add parent network to list, and mutate
+            nets.add(i, new NeuralNetwork(net));
+            nets.get(i).mutate();
 
-            // Mutate
-            //nets.get(i).mutate();
+            for (int k = 0; k < Math.pow(2, input.length); k++) {
 
-            // Reset deviation
-            deviation = 0;
-
-
-            // Randomly choose x numbers and see the deviation of them, x being 10
-            for (int k = 0; k < 10; k++) {
-                // Generate binary for decimal
-                randomNumber = (int) Math.floor((Math.random() * max) + min);
-                boolArray = toBin(randomNumber, inputNodes);
-                binArray = new float[inputNodes];
-                for (int j = 0; j < inputNodes; j++) {
-                    binArray[j] = (float) (boolArray[j] ? 1 : 0);
+                boolean[] boolArray;
+                boolArray = toBin(k, input.length);
+                for (int j = 0; j < input.length; j++) {
+                    input[j] = (float)(boolArray[j] ? 1 : 0);
                 }
+                // Get output
+                out = nets.get(i).feedForward(input);
 
-                // Convert the answers to negative or positive
-                for (int j = 0; j < max - 1; j++) {
-                    binArray[j] -= .5;
-                }
 
-                // Run through network
-                out = nets.get(i).feedForward(binArray);
-
-                // Basically turn out into negative or positive, then figure out the deviation
-                for (int j = 0; j < binArray.length; j++) {
-                    if (out[j] < 0) {
-                        out[j] = (float) -0.5;
-                    } else {
-                        out[j] = (float) 0.5;
+                // Turn out into 1s or 0s
+                for (int j = 0; j < out.length; j++) {
+                    if(out[j] <= 0){
+                        out[j] = 0;
+                    }else{
+                        out[j] = 1;
                     }
-                    // Formula is absoluteValueOf((firstDigitInBinArray - firstDigitInOut) * 2^theFirstDigitBinary)
-                    // Meaning if the bit of binArray[0] = 1, and out[0] = 0, it would be (1 - 0) * what place it is in binary, (this case is 2)
-                    // the way I store it is read from right to left, 10 = 2, and 01 = 1
-                    deviation += Math.abs((binArray[j] - out[j]) * Math.pow(2, binArray.length - 1 - j));
+                }
+
+                // Check if it does what I want it to do
+                for (int j = 0; j < out.length; j++) {
+                    if(out[j] == input[j]){
+                        nets.get(i).addFitness(1);
+                    }
                 }
             }
 
-            // Pick the next best algorithm
-            if (deviation < lastDeviation) {
+            // Check if this network is best
+            if(nets.get(i).getFitness() > nets.get(bestNetwork).getFitness()){
                 bestNetwork = i;
             }
-            // Remember the last deviation to see if this ai is better than the last one
-            lastDeviation = deviation;
         }
         return nets.get(bestNetwork);
-    }
-    static float[] toDec(){
-            return null;
     }
     private static boolean[] toBin(int number, int length) {
         final boolean[] boolArr = new boolean[length];
@@ -101,5 +104,8 @@ public class Main {
             boolArr[length - 1 - i] = (1 << i & number) != 0;
         }
         return boolArr;
+    }
+    static float[] toDec(){
+            return null;
     }
 }
--- src/NeuralNetwork.java
+++ src/NeuralNetwork.java
@@ -5,7 +5,7 @@ public class NeuralNetwork{
     // Middle layers not including start and end nodes
     private final int[] layers;
     private float[][] neurons;
-    private float[][][] weights;
+float[][][] weights;
     private float fitness;
     public NeuralNetwork(int[] layers) {
         //deep copy of layers of this network
@@ -26,9 +26,9 @@ public class NeuralNetwork{
     }
     private void copyWeights(float[][][] copyWeights){
         for(int i = 0; i < weights.length; i++){
-            // Loop over all neurons
+            // Loop over all weights
             for(int j = 0; j < weights[i].length; j++){
-                // Multiply the value of the previous neuron by the weights of this neuron
+                // Copy the weights
                 System.arraycopy(copyWeights[i][j], 0, weights[i][j], 0, weights[i][j].length);
             }
         }
@@ -55,7 +55,8 @@ public class NeuralNetwork{
 
                 for(int k = 0; k < layers[i + 1]; k++){
                     // Initialize them to a value of -.5 to .5
-                    weights[i][j][k] = (float) ((Math.random() - .5) * 2);
+                    //weights[i][j][k] = 1;
+                    weights[i][j][k] = (float) (Math.random() - .5);
                 }
             }
         }
@@ -64,53 +65,82 @@ public class NeuralNetwork{
         //System.out.println(Arrays.deepToString(weights));
     }
     public float[] feedForward(float[] inputs){
+        float value;
         // Go over the input layer and initialize the inputs to what we put
         System.arraycopy(inputs, 0, neurons[0], 0, inputs.length);
+
         // Loop over all layers except input
         for(int i = 1; i < layers.length; i++){
+
             // Loop over all neurons
             for(int j = 0; j < neurons[i].length; j++){
+                value = 0;
+
                 // Multiply the value of the previous neuron by the weights of this neuron
-                float value = 0.25f;
                 for(int k = 0; k < neurons[i - 1].length; k++){
                     value += weights[i - 1][j][k] * neurons[i - 1][k];
                 }
-                // IDK what tanh does
+
+                // IDK what tanh does, I followed a tutorial
+                // "hyperbolic tangent activation" what the heck does that mean
                 neurons[i][j] = (float)Math.tanh(value);
             }
         }
+
         // Return output layer
         return neurons[neurons.length - 1];
     }
     public void mutate(){
-        // For each column of neurons
-        for (int i = 0; i < weights.length; i++){
-            // For each row of neurons
-            for (int j = 0; j < weights[i].length; j++){
-                // For each weight
-                for (int k = 0; k < weights.length; k++) {
-                    // Mutate
-                    float weight = weights[i][j][k];
-                    int randomNumber = (int) (Math.random() * 100);
-                    if (randomNumber <= 5) {
-                        weight *= -1;
-                    } else if (randomNumber <= 10) {
-                        weight = (float) (Math.random() - .5);
+        float weight;
+        float factor;
+        int randomNumber;
+            for (int i = 0; i < weights.length; i++) {
+                for (int j = 0; j < weights[i].length; j++) {
+                    for (int k = 0; k < weights[i][j].length; k++) {
+                        weight = weights[i][j][k];
+
+                        //mutate weight value
+                        randomNumber = (int) (Math.random() * 100);
+
+                        if (randomNumber <= 2f) { //if 1
+                            //flip sign of weight
+                            weight *= -1f;
+                        } else if (randomNumber <= 4f) { //if 2
+                            //pick random weight between -1 and 1
+                            weight = (float) (Math.random() - .5);
+                        } else if (randomNumber <= 6f) { //if 3
+                            //randomly increase by 0% to 100%
+                            factor = (float) (Math.random() + 1);
+                            weight *= factor;
+                        } else if (randomNumber <= 8f) { //if 4
+                            //randomly decrease by 0% to 100%
+                            factor = (float)Math.random();
+                            weight *= factor;
+                        }
+                        weights[i][j][k] = weight;
                     }
-                    weights[i][j][k] = weight;
                 }
             }
         }
-    }
-    public void AddFitness(float fit) {
+    public void addFitness(float fit) {
         fitness += fit;
     }
 
-    public void SetFitness(float fit) {
+    public void setFitness(float fit) {
         fitness = fit;
     }
 
-    public float GetFitness() {
+    public float getFitness() {
         return fitness;
+    }
+    public int compareTo(NeuralNetwork other) {
+        if (other == null) return 1;
+
+        if (fitness > other.fitness)
+            return 1;
+        else if (fitness < other.fitness)
+            return -1;
+        else
+            return 0;
     }
 }
